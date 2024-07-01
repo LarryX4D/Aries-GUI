2548065a8fb5eba9dbc33ffa69dd6f1932efb11d
faisal1432152 <166968571+faisal1432152@users.noreply.github.com>
2024-07-01 12:41:27 +0300

    Update Emoji.lua

--- Emoji.lua
+++ Emoji.lua
@@ -44,7 +44,7 @@ function premium()
 		elseif SkidId[v.UserId] then
 			if v.Character then
 				if v.Character.Parent.Name == 'Players' then
-					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('💩'..v.DisplayName)
+					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💩]'..v.DisplayName)
 				end
 			end
         elseif ThugId[v.UserId] then
